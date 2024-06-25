986d87aae06eead8623636d97408bbb954dcee28
Ernest Guevarra <ernest@guevarra.io>
2024-06-25 18:33:52 +0100

    continue eda

--- R/summarise_data.R
+++ R/summarise_data.R
@@ -202,3 +202,187 @@ summarise_anc_data_univariate <- function(anc_data_recode,
   summary_tab
 }
 
+
+#'
+#'
+#'
+
+summarise_anc_data_bivariate <- function(anc_data_recode,
+                                        simplify = TRUE) {
+  summary_tab <- list(
+    `Age` = anc_data_recode |>
+      dplyr::group_by(anaemia_status) |>
+      dplyr::summarise(
+        `Median Age` = median(age, na.rm = TRUE), 
+        `Confidence Interval` = boxplot.stats(age)$conf |>
+          round(digits = 1) |>
+          paste(collapse = ", "),
+        .groups = "drop"
+      ) |>
+      tidyr::pivot_wider(
+        names_from = anaemia_status, 
+        values_from = `Median Age`:`Confidence Interval`
+      )
+      
+      
+      dplyr::mutate(conf_int = boxplot.stats())
+       |>
+      cbind(
+        anc_data_recode |>
+          dplyr::summarise(
+            conf_int_age = boxplot.stats(age)$conf |>
+              round(digits = 1) |>
+              paste(collapse = ", ")
+          ) |>
+          tidyr::pivot_longer(
+            cols = dplyr::everything(),
+            names_to = "var",
+            values_to = "value2"
+          ) |>
+          dplyr::select(value2)
+      ) |>
+      dplyr::mutate(value = paste0(value1, " (", value2, ")")) |>
+      dplyr::select(var, value),
+    `Age Group` = anc_data_recode |>
+      dplyr::count(age_group) |>
+      dplyr::arrange(dplyr::desc(n)) |>
+      dplyr::mutate(
+        n = paste0(
+          n, " (", 
+          scales::label_percent()(n / unique(anc_data_recode$total_n)),
+          ")"
+        )
+      ) |>
+      dplyr::rename(var = age_group, value = n),
+    `Haemoglobin` = anc_data_recode |>
+      dplyr::summarise(
+        `Median Hb` = median(haemoglobin, na.rm = TRUE)
+      ) |>
+      tidyr::pivot_longer(
+        cols = dplyr::everything(),
+        names_to = "var",
+        values_to = "value1"
+      ) |>
+      cbind(
+        anc_data_recode |>
+          dplyr::summarise(
+            conf_int_age = boxplot.stats(haemoglobin)$conf |>
+              round(digits = 1) |>
+              paste(collapse = ", ")
+          ) |>
+          tidyr::pivot_longer(
+            cols = dplyr::everything(),
+            names_to = "var",
+            values_to = "value2"
+          ) |>
+          dplyr::select(value2)
+      ) |>
+      dplyr::mutate(value = paste0(value1, " (", value2, ")")) |>
+      dplyr::select(var, value),
+    `Anaemia Status` = anc_data_recode |>
+      dplyr::count(anaemia_status) |>
+      dplyr::arrange(dplyr::desc(n)) |>
+      dplyr::mutate(
+        n = paste0(
+          n, " (", 
+          scales::label_percent()(n / unique(anc_data_recode$total_n)),
+          ")"
+        )
+      ) |>
+      dplyr::rename(var = anaemia_status, value = n),
+    `Anaemia Category` = anc_data_recode |>
+      dplyr::count(anaemia_category) |>
+      dplyr::arrange(dplyr::desc(n)) |>
+      dplyr::mutate(
+        n = paste0(
+          n, " (", 
+          scales::label_percent()(n / unique(anc_data_recode$total_n)),
+          ")"
+        )
+      ) |>
+      dplyr::rename(var = anaemia_category, value = n),
+    Profession = anc_data_recode |>
+      dplyr::count(profession) |>
+      dplyr::arrange(dplyr::desc(n)) |>
+      dplyr::mutate(
+        n = paste0(
+          n, " (", 
+          scales::label_percent()(n / unique(anc_data_recode$total_n)),
+          ")"
+        )
+      ) |>
+      dplyr::rename(var = profession, value = n),
+    `Education Level` = anc_data_recode |>
+      dplyr::count(education_level) |>
+      dplyr::arrange(dplyr::desc(n)) |>
+      dplyr::mutate(
+        n = paste0(
+          n, " (", 
+          scales::label_percent()(n / unique(anc_data_recode$total_n)),
+          ")"
+        )
+      ) |>
+      dplyr::rename(var = education_level, value = n),
+    `Marital Status` = anc_data_recode |>
+      dplyr::count(marital_status) |>
+      dplyr::arrange(dplyr::desc(n)) |>
+      dplyr::mutate(
+        n = paste0(
+          n, " (", 
+          scales::label_percent()(n / unique(anc_data_recode$total_n)),
+          ")"
+        )
+      ) |>
+      dplyr::rename(var = marital_status, value = n),
+    `Address` = anc_data_recode |>
+      dplyr::count(address) |>
+      dplyr::arrange(dplyr::desc(n)) |>
+      dplyr::mutate(
+        n = paste0(
+          n, " (", 
+          scales::label_percent()(n / unique(anc_data_recode$total_n)),
+          ")"
+        )
+      ) |>
+      dplyr::rename(var = address, value = n),
+    `Sickle Cell` = anc_data_recode |>
+      dplyr::count(sickle_cell) |>
+      dplyr::arrange(dplyr::desc(n)) |>
+      dplyr::mutate(
+        n = paste0(
+          n, " (", 
+          scales::label_percent()(n / unique(anc_data_recode$total_n)),
+          ")"
+        )
+      ) |>
+      dplyr::rename(var = sickle_cell, value = n),
+    `Malaria` = anc_data_recode |>
+      dplyr::count(malaria) |>
+      dplyr::arrange(dplyr::desc(n)) |>
+      dplyr::mutate(
+        n = paste0(
+          n, " (", 
+          scales::label_percent()(n / unique(anc_data_recode$total_n)),
+          ")"
+        )
+      ) |>
+      dplyr::rename(var = malaria, value = n)
+  )
+  
+  summary_tab <- lapply(
+    X = summary_tab,
+    FUN = function(x) tibble::tibble(
+      rbind(
+        x[!is.na(x$var), ],
+        x[is.na(x$var), ]
+      )
+    )
+  )
+  
+  if (simplify) {
+    summary_tab <- summary_tab |>
+      dplyr::bind_rows(.id = "var_set")
+  }
+  
+  summary_tab
+}
