c8d15e6961db4f5726204f3055b2901d10ffd09d
Ernest Guevarra <ernest@guevarra.io>
2024-08-01 08:17:17 +0100

    update Rmds to save plots in specific location

--- R/create_outputs.R
+++ R/create_outputs.R
@@ -29,19 +29,17 @@ create_xlsx_output <- function(anc_odds_ratio_table,
                                anc_logit_model_summary,
                                anc_gaussian_model_summary,
                                path) {
-  wb <- openxlsx::createWorkbook()
+  output_tables <- list(
+    "odds_ratio" = anc_odds_ratio_table,
+    "t_test" = anc_t_test_table,
+    "logit_model" = anc_logit_model_summary,
+    "gaussian_model" = anc_gaussian_model_summary
+  )
   
-  openxlsx::addWorksheet(wb, "odds_ratio")
-  openxlsx::writeData(wb, "odds_ratio", anc_odds_ratio_table)
-  
-  openxlsx::addWorksheet(wb, "t_test")
-  openxlsx::writeData(wb, "t_test", anc_t_test_table)
-  
-  openxlsx::addWorksheet(wb, "logit_model")
-  openxlsx::writeData(wb, "logit_model", anc_logit_model_summary)
-  
-  openxlsx::addWorksheet(wb, "gaussian_model")
-  openxlsx::writeData(wb, "gaussian_model", anc_gaussian_model_summary)
+  wb <- openxlsx::buildWorkbook(
+    x = output_tables,
+    colWidths = "auto"
+  )
   
   openxlsx::saveWorkbook(wb = wb, file = path, overwrite = TRUE)
   
